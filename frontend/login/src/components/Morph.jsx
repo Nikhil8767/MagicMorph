e832ee79bfea8c965393b2c7b8da2a266ca0dcb6
Nikhil <nikhilgupta932003@gmail.com>
2025-07-03 14:35:26 +0530

    uodated morph

--- frontend/login/src/components/Morph.jsx
+++ frontend/login/src/components/Morph.jsx
@@ -1,11 +1,58 @@
-import react from 'react'
+import React, { useRef, useState } from 'react';
+import CanvasDraw from 'react-canvas-draw';
 
-const Morph=()=>{
+const Morph = () => {
+  const canvasRef = useRef(null);
+  const [color,setColor]=useState('#000000');
+    const [size,setSize]=useState(4);
+    const [eraser,setEraser]=useState(false);
+
+  const handleSave = () => {
+    const data = canvasRef.current.getSaveData();
+    alert('the update is coming soon..')
+    console.log("Saved Drawing:", data);
+     
     
-    return(
-        <>
-        <h2>MORPH</h2>
-        </>
-    )
-}
-export default Morph
+  };
+
+  const handleClear = () => {
+    canvasRef.current.clear();
+  };
+  const ToggleEraser=()=>{
+    setEraser(!eraser)
+  }
+  const disableEraser=()=>{
+    setEraser(false);
+  }
+
+  return (
+    <div className='flex flex-col items-center mt-10'>
+      <h1 className='text-2xl font-bold mb-4'>Draw Anything You Like ✏️</h1>
+      <CanvasDraw 
+        ref={canvasRef}
+        brushRadius={size}
+        brushColor={eraser ? '#FFFFFF':color}
+        canvasWidth={1000}
+        canvasHeight={500}
+        lazyRadius={1}
+        hideGrid
+        className="border shadow-lg rounded-lg"
+      />
+      <div className='flex gap-4 mt-4'>
+        <label>
+            color:
+            <input className='border rounded' type="color" placeholder='enter the color you want' value={color} onChange={(e)=>{setColor(e.target.value); setEraser(false) }} disabled={eraser}/>
+        </label>
+        <label>brush:
+            <input className='border rounded' type="range" min="2" max="46" placeholder='enter the size you  want' value={size} onChange={(e)=>setSize(e.target.value)} />
+        </label>
+        <button onClick={()=>setEraser(!eraser)} className={`${eraser ? 'bg-yellow-300':'bg-blue-500'} text-whitepx-4 py-2 rounded`}>{eraser? 'eraser on':'eraser off'}</button>
+        
+        <button onClick={handleSave} className='bg-green-500 text-white px-4 py-2 rounded'>Save</button>
+        <button onClick={handleClear} className='bg-red-500 text-white px-4 py-2 rounded'>Clear</button>
+      </div>
+    </div>
+  );
+};
+
+export default Morph;
